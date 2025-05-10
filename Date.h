f4fcdb4c08413d86db180fcf358f6a0b9d9a5549
jjw86297 <jjw86297@email.vccs.edu>
2025-05-10 00:14:37 -0400

    N/A

--- Date.h
+++ Date.h
@@ -1,5 +1,36 @@
-#pragma once
+#ifndef DATE_H
+#define DATE_H
+
 class Date
 {
+	private:
+		int day;
+		int month;
+		int year;
+
+		bool inputValid(int, int, int);
+
+	public:
+		Date(int = 1, int = 1, int = 1900);
+
+		void setDate(int d, int m, int y);
+
+		int getDay() const
+			{ return day; }
+		int getMonth() const
+			{ return month; }
+		int getYear() const
+			{ return year; }
+
+		bool isLeapYear();
+		bool isLeapYear(int year);
+
+		int lastDay();
+		int lastDay(int month, int year);
+
+		void numberPrint();
+		void commaPrint();
+		void dmyPrint();
 };
 
+#endif
